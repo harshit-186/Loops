6aaabf67b37bac912248785721a04b2a4b1a96f1
Harshit Yadav <yaduvanshi8186@gmail.com>
2026-05-13 12:46:36 +0530

    Add files via upload

---
+++ Whilee.java
@@ -0,0 +1,15 @@
+
+// import java.net.SocketTimeoutException;
+// import javax.xml.transform.Source;
+
+public class Whilee {
+    public static void main(String[] args) {
+        //printing the natural no. from 1 to 10
+        int i=1;
+        while(i<=10){
+        System.out.println(i);
+        i++;
+        }
+    }
+    
+}
